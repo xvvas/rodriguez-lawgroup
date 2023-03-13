25118a97f16e2eae7dd2e864dda8803f084148ea
xvvas <miguelvg097@gmail.com>
2023-03-13 18:01:32 -0500

    Add basic Structure

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='nav'>
+
+      </div>
+      <div className='main'>
+        
+      </div>
+      <div className='bio'>
+
+      </div>
+      <div className='cta'>
+
+      </div>
+      <div className='cases'>
+
+      </div>
+      <div className='contact'>
+
+      </div>
+      <div className='footer'>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
