9f5ff29dafabaa05b5c7e37a959581792ea26343
xvvas <miguelvg097@gmail.com>
2023-03-14 18:01:49 -0500

    add Schema for react router menu .

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link, } from "react-router-dom";
 import Nav from './Components/main-nav/nav-container';
-
+import Home from './Components/Pages/home'
 function App() {
   return (
     <div className="App">
@@ -8,23 +9,12 @@ function App() {
         <Nav/>
       </div>
     {/* Esto en un futuro va a parte para acomodar el React Router */}
-      <div className='main'>
-        
-      </div>
-      <div className='bio'>
-
-      </div>
-      <div className='cta'>
-
-      </div>
-      <div className='cases'>
-
-      </div>
-      <div className='contact'>
-
-      </div>
-      <div className='footer'>
-
+    <Router>
+      <Routes>
+        <Route path='/' exact component={Home} />
+      </Routes>
+    </Router>
+    <div className='footer'>
       </div>
     </div>
   );
---
+++ src/Components/Pages/home.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+function Home() {
+    return (
+        <>
+        <div className='main'>
+        
+        </div>
+        <div className='bio'>
+  
+        </div>
+        <div className='cta'>
+  
+        </div>
+        <div className='cases'>
+  
+        </div>
+        <div className='contact'>
+  
+        </div>
+        </>
+    )
+  }
+
+  export default Home;
