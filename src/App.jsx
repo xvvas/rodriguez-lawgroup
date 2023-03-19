180ef27946ba3adfe1c42ffdd413e08f3c5aacb0
xvvas <miguelvg097@gmail.com>
2023-03-18 21:48:46 -0500

    finish routing schema and work in home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import './App.css';
-import {BrowserRouter as Router, Routes, Route, } from "react-router-dom";
+import {BrowserRouter, Routes, Route, } from "react-router-dom";
 import Nav from './Components/main-nav/Js/nav-container';
-import Home from './Components/Pages/home'
+import Home from './Components/Pages/home/js/home'
 function App() {
   return (
-    <div className="App">
-        <Nav/>
-      <Router basename='/'>
-        <Routes>
-          <Route path='/' exact component={Home} />
-        </Routes>
-      </Router>
-    <div className='footer'>
+    <BrowserRouter>
+      <div className="App">
+          <Nav/>
+          <Routes>
+            <Route path='/' 
+            element={<Home />} />
+          </Routes>
+        <div className='footer'>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/Pages/home/js/home.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import '../css/home.css'
+function Home() {
+    return (
+        <>
+            <div className='main'>
+                <h1>testing</h1>
+            </div>
+            <div className='bio'>
+    
+            </div>
+            <div className='cta'>
+    
+            </div>
+            <div className='cases'>
+    
+            </div>
+            <div className='contact'>
+    
+            </div>
+        </>
+    )
+  }
+
+  export default Home;
